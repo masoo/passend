5621e9e1f779551678737dcf2147678bebb9d2fb
FUNABARA Masao <masao@masoo.jp>
2014-09-16 15:14:46 +0900

    add Message Scaffold.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140916053915) do
+ActiveRecord::Schema.define(version: 20140916060947) do
+
+  create_table "messages", force: true do |t|
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false

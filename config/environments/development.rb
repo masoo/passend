79c24acf0fb1cefa8a1020a9c2ecb8ec5c3dfe74
FUNABARA Masao <masao@masoo.jp>
2018-01-05 13:00:38 +0900

    setting mailtrap for development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,16 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => Rails.application.secrets.mailtrap[:user_name],
+    :password => Rails.application.secrets.mailtrap[:password],
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 

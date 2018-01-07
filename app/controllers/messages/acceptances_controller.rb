6b4a36eec2ce1f648bdbc3f14a9036fe55fbfb99
FUNABARA Masao <masao@masoo.jp>
2018-01-07 23:03:15 +0900

    add acceptance policy.

--- app/controllers/messages/acceptances_controller.rb
+++ app/controllers/messages/acceptances_controller.rb
@@ -1,5 +1,6 @@
 class Messages::AcceptancesController < ApplicationController
   def index
+    authorize Acceptance.new
     authentication = current_user
     @acceptances = Acceptance.where(email: authentication.email)
   end
